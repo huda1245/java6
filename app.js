0b6e7f65e198a0d10639d033429dac22a5276e1a
hudashahzad <hudashahzad786@gmail.com>
2022-07-25 02:00:48 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// task 1 remove comment in design
+// function sub(){
+
+//     var name=document.getElementById("inputName");
+//     var email=document.getElementById("inputEmail4");
+//     var password=document.getElementById("inputAddress");
+
+//     document.write("SignUp complete <br>")
+//     document.write("Name: "+name.value  +"<br>")
+//     document.write("Email: "+email.value  +"<br>")
+//     document.write("Password: "+password.value  +"<br>")   
+// }
+
+// function p_fun(){
+//     var text="hello world how are you, i hope you are doing well i will see you soon when i done with work";
+//     var p=document.getElementById("p-tag");
+//     p.innerHTML=text;
+//     }
+
